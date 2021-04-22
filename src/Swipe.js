937b0eacef2d80297ba9495a4a1863c01a5b722e
Monty Gaurav khurana <montygauravkhurana@Montys-MacBook-Air.local>
2021-04-22 12:28:04 -0300

    mouse and touch swipe detection

--- src/Swipe.js
+++ src/Swipe.js
@@ -1,19 +1,17 @@
-import React, { memo, useReducer, useRef, useEffect } from "react";
+import React, { memo, useReducer, useRef, useEffect, useCallback } from "react";
 
 //--------------------------
 // constants
 //--------------------------
 
 const initialState = {
-  canMove: false,
-  touchDown: { X: 0, Y: 0 },
-  prevX: 0,
-  prevY: 0,
   gesture: "",
   allowedTime: 200,
   threshold: 150,
-  elapsedTime: 0,
-  startTime: 0
+  timeOfLastDragEvent: 0,
+  touchStartX: 0,
+  touchStartY: 0,
+  beingTouched: false,
 };
 
 //--------------------------
@@ -21,44 +19,24 @@ const initialState = {
 //--------------------------
 function reducer(state, action) {
   switch (action.type) {
-    case "canMove":
-      return {
-        ...state,
-        ...{ canMove: action.value }
-      };
+
     case "gesture":
       debugger;
       return {
         ...state,
         ...{ gesture: action.value }
       };
-    case "prevX":
+    case "start":
+    case "end":
       return {
         ...state,
-        ...{ prevX: action.value }
-      };
-    case "prevY":
+        ...action.value
+      } 
+    case "captureTime":
       return {
         ...state,
-        ...{ prevY: action.value }
-      };
-    case "touchDown":
-      return {
-        ...state,
-        ...{ touchDownX: action.value },
-        ...{ startTime: action.startTime }
-      };
-    case "touchEnd":
-      return {
-        ...state,
-        ...{
-          canMove: false,
-          touchDown: { X: 0, Y: 0 },
-          prevX: 0,
-          gesture: "",
-          prevY: 0
-        }
-      };
+        ...action.value
+      }  
     default:
       state;
   }
@@ -67,15 +45,91 @@ function reducer(state, action) {
 function Swipe({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const {
-    canMove,
-    prevX,
-    touchDown,
-    threshold,
-    allowedTime,
-    elapsedTime,
-    prevY
+    beingTouched,
+    timeOfLastDragEvent,
+    touchStartX,
+    touchStartY
   } = state;
   const swipeRef = useRef();
+
+
+  //-----------------------------------
+  // Swiping logic
+  //-----------------------------------
+  /* Mouse handlers  start*/
+
+  const handleStart = useCallback((e)=>{
+    console.log('start', e.clientX, e.clientY);
+    dispatch({type: 'start', value: {
+        touchStartX: e.clientX,
+        touchStartY: e.clientY,
+        beingTouched: true,
+        timeOfLastDragEvent: Date.now(),
+      } 
+    });
+  },[]);
+  const handleEnd = useCallback(()=>{
+    console.log('start');
+    dispatch({type:'end', value: {
+      beingTouched: false,
+      touchStartX: 0,
+      touchStartY: 0,
+    }})
+  },[])
+  const handleMouseDown = useCallback((e)=>{
+    console.log('mouse down');
+    e.preventDefault();
+    handleStart(e);
+  },[]);
+  const handleMouseUp = useCallback((e)=>{
+    console.log('mouse up');
+    handleEnd(e);
+  },[]);
+  const handleMouseLeave = useCallback((e)=>{
+    console.log('mouse leave');
+    handleEnd(e);
+  },[]);
+
+
+  const handleMove = useCallback((e) => {
+    
+    if (beingTouched){
+      const currTime = Date.now();
+      if (e.pageX < touchStartX) {
+          /* to left */
+          dispatch({ type: "gesture", value: "Swiping left!" });
+        } else if (e.pageX > touchStartX ) {
+          /* to right */
+          dispatch({ type: "gesture", value: "Swiping right!" });
+        } else if (e.pageY > touchStartY) {
+          /* to Bottom */
+          dispatch({ type: "gesture", value: "Swiping Down!" });
+        } else if (e.pageY < touchStartY) {
+          /* to Top */
+          dispatch({ type: "gesture", value: "Swiping Up!" });
+        }
+      dispatch({type: 'captureTime', value : { timeOfLastDragEvent: currTime, touchStartX: e.pageX, touchStartY:e.pageY }})
+
+    }
+  },[beingTouched, timeOfLastDragEvent, touchStartX, touchStartY]);
+
+  /* Mouse handlers  ends*/
+
+  /* Touch handlers  start*/
+  const handleTouchStart = useCallback((e) => {
+    e.preventDefault();
+    handleStart(targetTouches[0]);
+  },[]);
+
+  const handleTouchMove = useCallback((e) => {
+    handleMove(e.targetTouches[0]);
+    dispatch({ type: "touchEnd" });
+  },[]);
+
+  const handleTouchEnd = useCallback((e) => {
+    handleEnd();
+  },[]);
+
 
   //---------------------------
   // On load
@@ -87,107 +141,21 @@ function Swipe({ children }) {
       element.addEventListener("touchstart", handleTouchStart);
       element.addEventListener("touchmove", handleTouchMove);
       element.addEventListener("touchend", handleTouchEnd);
-      element.addEventListener("mousedown", handleMouse);
-      element.addEventListener("mouseup", handleMouse);
+      element.addEventListener("mouseup", handleMouseUp);
+      element.addEventListener("mousedown", handleMouseDown);
       element.addEventListener("mousemove", handleMove);
+      element.addEventListener("mouseleave", handleMouseLeave);
       return () => {
         element.removeEventListener("touchstart", handleTouchStart);
         element.removeEventListener("touchmove", handleTouchMove);
         element.removeEventListener("touchend", handleTouchEnd);
-        element.addEventListener("mousedown", handleMouse);
-        element.addEventListener("mouseup", handleMouse);
-        element.addEventListener("mousemove", handleMove);
+        element.removeEventListener("mousedown", handleMouseDown);
+        element.removeEventListener("mouseup", handleMouseUp);
+        element.removeEventListener("mousemove", handleMove);
+        element.removeEventListener("mouseleave", handleMouseLeave);
       };
     }
-  }, []);
-
-  //-----------------------------------
-  // Swiping logic
-  //-----------------------------------
-  /* Mouse handlers  start*/
-  const handleMouse = (e) => {
-    if (e.type === "mouseup") {
-      dispatch({ type: "canMove", value: false });
-    } else {
-      dispatch({ type: "canMove", value: true });
-    }
-  };
-  const handleMove = (e) => {
-    debugger;
-    if (e.pageX < prevX && canMove) {
-      debugger;
-      /* to left */
-      dispatch({ type: "gesture", value: "Swiping left!" });
-      dispatch({ type: "canMove", value: false });
-    } else if (e.pageX > prevX && canMove) {
-      debugger;
-      /* to right */
-      dispatch({ type: "gesture", value: "Swiping right!" });
-      dispatch({ type: "canMove", value: false });
-    } else if (e.pageY > prevY && canMove) {
-      /* to Bottom */
-
-      dispatch({ type: "gesture", value: "Swiping Down!" });
-      dispatch({ type: "canMove", value: false });
-    } else if (e.pageY < prevY && canMove) {
-      /* to Top */
-
-      dispatch({ type: "gesture", value: "Swiping Up!" });
-      dispatch({ type: "canMove", value: false });
-    }
-    dispatch({ type: "prevX", value: e.pageX });
-    dispatch({ type: "prevY", value: e.pageY });
-  };
-
-  /* Mouse handlers  ends*/
-
-  /* Touch handlers  start*/
-  const handleTouchStart = (e) => {
-    dispatch({
-      type: "touchDown",
-      value: { X: e.touches[0].clientX, Y: e.touches[0].clientY },
-      startTime: new Date().getTime()
-    });
-  };
-
-  const handleTouchMove = (e) => {
-    if (!touchDown || !touchDown.X || !touchDown.Y) {
-      return;
-    }
-
-    const xUp = e.touches[0].clientX;
-    const yUp = e.touches[0].clientY;
-
-    const xDiff = touchDown.X - xUp;
-    const yDiff = touchDown.Y - yUp;
-    const elapsedTime = new Date().getTime();
-    if (elapsedTime <= allowedTime) {
-      if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        /*most significant*/
-        if (xDiff > 0) {
-          /* left swipe */
-          dispatch({ type: "gesture", value: "Swiping left!" });
-        } else {
-          dispatch({ type: "gesture", value: "Swiping right!" });
-          /* right swipe */
-        }
-      } else {
-        if (yDiff > 0) {
-          /* up swipe */
-
-          dispatch({ type: "gesture", value: "Swiping Up!" });
-        } else {
-          /* down swipe */
-          dispatch({ type: "gesture", value: "Swiping Down!" });
-        }
-      }
-    }
-    dispatch({ type: "touchEnd" });
-  };
-
-  const handleTouchEnd = (event) => {
-    dispatch({ type: "touchEnd" });
-  };
+  }, [handleMove, handleTouchStart, handleTouchMove, handleMouseDown, handleMouseDown, handleMouseLeave]);
 
   return (
     (children && (
